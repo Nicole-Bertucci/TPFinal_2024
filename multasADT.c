849cc1179455d760a165f5395007471d479e2d32
Nicole-Bertucci <nbertucci@itba.edu.ar>
2024-07-01 17:18:25 -0300

    Update multasADT.c

--- multasADT.c
+++ multasADT.c
@@ -1,3 +1,46 @@
 #include <stdio.h>
 #include <ctype.h>
 
+typedef struct tInfraction{
+    char* name;
+    int cant;
+}tInfraction;
+
+typedef struct tAgency{
+    struct tAgency * next;
+    char* name;
+    tInfraction * infractions;
+}tAgency;
+
+typedef struct multasADT{
+tInfraction * infractions;
+tAgency * first;
+tAgency* iter;
+}multasADT;
+
+
+// odenadas alfabeticamente 
+// typedef struct tAgency{
+//     tMulta* firstFine;
+//     char* nameAgency;
+//     char* infPop;
+//     int cantInfPop;
+//     struct tAgency nextAgency;
+// }tAgency;
+
+// typedef struct tInfraction{
+//     unsigned int id;
+//     char* name;
+//     int totalInf;
+//     char* popPlate;
+//     int cantPopPlate;
+//     struct tInfraction * nextInf
+// } tInfraction;
+
+// typedef struct fineCDT{
+// tAgency * firstAgency;
+// tAgency * iterAgency;
+// tInfraction * firstInfraction;
+// tInfraction * iterInfraction;
+// }fineCDT;
+
