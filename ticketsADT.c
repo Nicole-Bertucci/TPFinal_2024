642c7721637f6cdf63bd9452a512ab0c1af976d9
Nicole-Bertucci <nbertucci@itba.edu.ar>
2024-07-02 05:00:30 -0300

    Update ticketsADT.c

--- ticketsADT.c
+++ ticketsADT.c
@@ -7,16 +7,18 @@
 
 #define ERRORMEMORIA "Error de asignacion de memoria"
 #define DATOINVALIDO "Dato ingresado es invalido"
-#define AGENCIAINVALIDA "El nombre de agencia es invalido"
 
 typedef struct tMulta{
+    struct tMulta* izq;
+    struct tMulta *der;
     char* plate;
     size_t cantidad;
 }tMulta;
 
 typedef struct tInfraction{
     char* nameInfr;
-    tMulta * multas;
+   // tMulta * multas;
+    tMulta * firstMulta;
     size_t dimMultas;
     size_t multasTotales;
 }tInfraction;
@@ -57,13 +59,13 @@ static tAgency * addAgencyRec(tAgency * agency, size_t id, char * name, size_t *
     int c;
     if (agency == NULL || (c = strcasecmp(agency->nameAgency, name)) > 0) {
         tAgency * new = malloc(sizeof(*new));
-        if(new = NULL){
+        if(new == NULL){
             perror(ERRORMEMORIA);
             exit(EXIT_FAILURE);            
         }
         if (*dim < id) {
             new->infractionsPopularity = malloc(id*sizeof(size_t));
-            if(new->infractionsPopularity = NULL){
+            if(new->infractionsPopularity == NULL){
                 perror(ERRORMEMORIA);
                 exit(EXIT_FAILURE);            
             }
@@ -111,49 +113,97 @@ void addAgency (ticketsADT ticket,  size_t id, char * name){
 void addInfraction(ticketsADT ticket, size_t id, const char* name){
     if(ticket->dimInfraction<=id-1){
        ticket->infractions= realloc(ticket->infractions, sizeof(tInfraction)*(id));
+       ticket->dimInfraction=id;
     }
     if(ticket->infractions[id-1].nameInfr==NULL){
         ticket->infractions[id-1].nameInfr=stringCopy(name);
         ticket->infractions[id-1].dimMultas=0;
         ticket->infractions[id-1].multasTotales=0;
-        ticket->infractions[id-1].multas=NULL;
+       // ticket->infractions[id-1].multas=NULL;
+       ticket->infractions[id-1].firstMulta=NULL;
     }
 
+
 }
 
-
-//@param ticket ciudad en donde ocurren las multas
-//@param idInfraccion numero de identificacion de la infraccion
-//@param patente numero de patente a la que se le aplico la multa
-//@param nombre de la agencia que escribe la multa
-// Si la patente no existe en el arreglo agrega al final del arreglo la multa 
-//segun la infraccion dada en @param idInfraccion, en el caso contrario solo
-//aumenta la cantidad de multas de esa patente
-// Aumenta la cantidad de multas de ese tipo de infraccion
-//Aumenta la cantidad de infracciones del tipo generado por la agencia  de nombre agency
-void addMulta(ticketsADT ticket, size_t idInfraction, const char* patente, char* agencyName ){
-if(ticket->dimInfraction<idInfraction){
-    perror(DATOINVALIDO);
-    exit(EXIT_FAILURE);
-}
-int index=!exist(idInfraction, patente);
-    if(!index){
-     ticket->infractions[idInfraction-1].multas=realloc(ticket->infractions[idInfraction-1].multas, sizeof(tMulta)*(ticket->infractions[idInfraction-1].dimMultas+1));
-    ticket->infractions[idInfraction-1].dimMultas++;
-    index=ticket->infractions[idInfraction-1].dimMultas;
-    ticket->infractions[idInfraction-1].multas[index].plate= stringCopy(patente);
-}
-    ticket->infractions[idInfraction-1].multas[index-1].cantidad++;
-    ticket->infractions[idInfraction-1].multasTotales++;
-    addAgency(ticket, idInfraction, agencyName);
-}
 
 
 //devuelve el index +1 del arreglo donde esta la patente 
 //o en el caso de lista deja el iterador ahi y devuelve 1 si existe 0 si no
-static int exist(int id, char* plateNumber){
+// static int exist(int id, char* plateNumber){
+
+// }
+
+//****VERSION DE ADDMULTA CON ARBOL */
+//si encuentra la patente agrega uno a su cantidad, caso contrario agrega la patente
+//en el lugar correspondiente siguiendo el orden alfabetico;
+static void addMultaRec(tMulta * firstMulta, const char* patente){
+    printf("entre\n");
+
+    if(firstMulta==NULL){
+        printf("aca llegue\n");
+        tMulta* new=malloc(sizeof(tMulta));
+        new->plate= stringCopy(patente);
+        new->cantidad=1;
+        new->der=NULL;
+        new->izq=NULL;
+        firstMulta=new;
+        return;
+    }
+    printf("me meti aca\n ");
+    if(strcmp(firstMulta->plate,patente )<0){
+        printf("pase por aca <0\n");
+      addMultaRec(firstMulta->der, patente);
+    }
+     printf("pase por aca\n ");
+    if(strcmp(firstMulta->plate,patente )>0){
+        printf("UWUUU\n ");
+     addMultaRec(firstMulta->izq, patente);
+    }
+    firstMulta->cantidad++;
 
 }
+
+
+
+ void addMulta(ticketsADT ticket, size_t idInfraction, const char* patente, char* agencyName){
+    if(ticket->dimInfraction<idInfraction){
+        perror(DATOINVALIDO);
+        exit(EXIT_FAILURE);
+    }
+    if(ticket->infractions[idInfraction-1].firstMulta==NULL){
+        tMulta* new=malloc(sizeof(tMulta));
+        new->plate= stringCopy(patente);
+        new->cantidad=1;
+        new->der=NULL;
+        new->izq=NULL;
+        ticket->infractions[idInfraction-1].firstMulta=new;
+        return;
+    }
+addMultaRec(ticket->infractions[idInfraction-1].firstMulta, patente);
+ 
+    // addAgency(ticket,idInfraction, agencyName);
+ }
+//****VERSION DE ADDMULTA CON VECTORES */
+
+// void addMulta(ticketsADT ticket, size_t idInfraction, const char* patente, char* agencyName ){
+// if(ticket->dimInfraction<idInfraction){
+//     perror(DATOINVALIDO);
+//     exit(EXIT_FAILURE);
+// }
+// int index=!exist(idInfraction, patente);
+//     if(!index){
+//      ticket->infractions[idInfraction-1].multas=realloc(ticket->infractions[idInfraction-1].multas, sizeof(tMulta)*(ticket->infractions[idInfraction-1].dimMultas+1));
+//     ticket->infractions[idInfraction-1].dimMultas++;
+//     index=ticket->infractions[idInfraction-1].dimMultas;
+//     ticket->infractions[idInfraction-1].multas[index].plate= stringCopy(patente);
+// }
+//     ticket->infractions[idInfraction-1].multas[index-1].cantidad++;
+//     ticket->infractions[idInfraction-1].multasTotales++;
+//     addAgency(ticket, idInfraction, agencyName);
+// }
+
+
 
 
 
